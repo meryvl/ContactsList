247ec9d19dda395ed71f701dbcdf87cc458b9493
meryvl <meryvalerolaguna@gmail.com>
2023-04-14 08:41:34 +0000

    new commit

--- src/js/views/Contact.js
+++ src/js/views/Contact.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Navbar } from "../component/navbar.js";
 import { useAppContext } from "../store/context.js";
 import { Link } from "react-router-dom";
-
+import TargetaContacto from "../component/TargetaContacto.jsx";
 const Contact =()=>{
     const {store , actions} = useAppContext();
     const {
@@ -27,24 +27,10 @@ return(
 				</Link>
 			</div>
             </div>
-{listContacts.length == 0 ? (
-                <p className="fs-4"> You haven't got contacts </p>
-            ) : (
-    listContacts.map((item, index)=>{
-return(
-   <div className="m-3 "> 
-<div key={index.id} className="contactStyle row">
-<img className="fotoContact  col-4" src="https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/09/02/16621364623205.jpg"/>
-<div className="col-5 mt-3">
-<p>Nombre : {item.full_name}</p>
-<p>Email : {item.email}</p>
-<p>Adreess : {item.address}</p>
-<p>Phone : {item.phone}</p>
-</div>
-<span className="me-3 col-3 iconDelete"  onClick={()=> handleDeleteContact(item.full_name)}><i className="fa-solid fa-xmark"></i></span>
-</div>
-</div>
-)}))} 
+
+<TargetaContacto/>
+ 
+
 
 
 
